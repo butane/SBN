1a5aeb04b50e7849e16b73ed561553ae2c9f2a5b
Vivek Dinesh <vivekdinesh5@gmail.com>
2016-01-30 00:06:47 +0530

    Better time

--- SBN.js
+++ SBN.js
@@ -10,7 +10,13 @@ SBN.refreshDOM = function () {
 
 SBN.updateTime = function () {
     var d = new Date();
-    var timeString = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
+    hours = hours<10?"0"+hours:hours;
+    minutes = minutes<10?"0"+minutes:minutes;
+    seconds = seconds<10?"0"+seconds:seconds;
+    var timeString = hours + ":" + minutes + ":" + seconds;
     $('#currentTime').html(timeString);
 };
 
