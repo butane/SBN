426b54530b1a0eb487e4330e2d15d03abb103248
Vivek Dinesh <vivekdinesh5@gmail.com>
2016-01-29 23:59:47 +0530

    Live time update

--- SBN.js
+++ SBN.js
@@ -1,3 +1,17 @@
 $(function () {
     $(".stickynote").draggable({ containment: "parent", stack: ".stickynote" });
 });
+
+SBN = {};
+
+SBN.refreshDOM = function () {
+    SBN.updateTime();
+};
+
+SBN.updateTime = function () {
+    var d = new Date();
+    var timeString = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    $('#currentTime').html(timeString);
+};
+
+setInterval(SBN.refreshDOM, 1000);
