bc90d8458416bc32d1a29ac011e509c037d8f151
Vivek Dinesh <vivekdinesh5@gmail.com>
2016-01-30 16:17:12 +0530

    Add notes

--- SBN.js
+++ SBN.js
@@ -1,8 +1,12 @@
-$(function () {
-    $(".stickynote").draggable({ containment: "parent", stack: ".stickynote" });
+$('document').ready(function () {
+    $('#addNewBtn').on('click', SBN.showAddNewModal);
+    $('#createBtn').on('click', SBN.addStickyNote);
+    setInterval(SBN.refreshDOM, 1000);
 });
 
 SBN = {};
+
+SBN.data = new Array();
 
 SBN.refreshDOM = function () {
     SBN.updateTime();
@@ -20,4 +24,44 @@ SBN.updateTime = function () {
     $('#currentTime').html(timeString);
 };
 
-setInterval(SBN.refreshDOM, 1000);
+SBN.showAddNewModal = function () {
+    $('#noteTitle').val('');
+    $('#noteDescription').val('');
+    $('#addNoteModal').modal('show');
+};
+
+SBN.addStickyNote = function () {
+    var title = $('#noteTitle').val();
+    var description = $('#noteDescription').val();
+    if (title.length>0 || description.length>0) {
+        var sNote = {
+            title: title,
+            description: description
+        };
+        SBN.data.push(sNote);
+        $('#addNoteModal').modal('hide');
+        SBN.renderNotes();
+    }
+};
+
+SBN.__stickyNote = function (title, description) {
+    var sWrapper = $('<div>');
+    sWrapper.addClass('stickynote');
+    var sTitle = $('<center><h4>'+title+'</h4></center>');
+    var sDescription = $('<center>'+description+'</center>');
+    sWrapper.append(sTitle);
+    sWrapper.append($('<br>'));
+    sWrapper.append(sDescription);
+    return sWrapper;
+};
+
+SBN.renderNotes = function () {
+    $('#notesContainer').html('');
+    if (SBN.data.length > 0) {
+        var notes = SBN.data;
+        for (i in notes) {
+            $('#notesContainer').append(SBN.__stickyNote(notes[i].title, notes[i].description));
+        }
+    }
+    $(".stickynote").draggable({ containment: "parent", stack: ".stickynote" });
+};
